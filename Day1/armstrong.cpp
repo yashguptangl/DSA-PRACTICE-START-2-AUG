a1dcada72b3dcf7dbf6afad7cf66368d4a55be4f
Yash Gupta <yguptangl@gmail.com>
2025-08-02 23:25:14 +0530

    Day1

--- Day1/armstrong.cpp
+++ Day1/armstrong.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
+    int n , check , ans  = 0 , lastdig , count = 0 ;
+    cout<<"Enter the Number: ";
+    cin>>n;
 
+    int temp = n;
+    check = n ;
+
+    while(temp){
+        count++;
+        temp /= 10;
+    }
+
+    while(n){
+        lastdig = n % 10 ;
+        ans = pow(lastdig , count) + ans ;
+        n /= 10 ;
+    }
+    if(ans == check ) cout<<"Armstrong Nomber";
+    else cout<<"Not Armstrong Number";
 }
